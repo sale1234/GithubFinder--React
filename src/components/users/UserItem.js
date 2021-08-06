ce23ead5f3bac88325bcf8170beb25319c460cbd
sale1234 <jevremovicsasa@gmail.com>
2021-08-06 15:57:02 +0200

    axios installed and added Users component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-  render() {
-    return <div>UserItem</div>
-  }
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  // Destructuring, pulling data from values I sent
+
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
+      </div>
+    </div>
+  )
+}
+
+UserItem.Prototype = {
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem
