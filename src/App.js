fc6fed3074b3deb6a4952cc2e7eca307c46f1220
sale1234 <jevremovicsasa@gmail.com>
2021-08-06 19:32:09 +0200

    github api done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import './App.css'
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    // start with true, and then ask if it has finished, if it is, stop spinner
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users?client_id={process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     )
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,35 +1,14 @@
-import React, { Component } from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: 3,
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-    ],
-  }
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
@@ -37,10 +16,15 @@ class Users extends Component {
   }
 }
 
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+}
+
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
-  gap: '40px',
+  gap: '1rem',
 }
 
 export default Users
